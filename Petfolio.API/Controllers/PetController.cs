5641141caa1a94dbf31561645f246f8f4e3e0a02
thaynaradamazio <thaydamazio2015@gmail.com>
2024-12-16 15:06:00 -0300

    feat: add project organization into class library

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pet.Delete;
+using Petfolio.Application.UseCases.Pet.GetAll;
+using Petfolio.Application.UseCases.Pet.GetById;
 using Petfolio.Application.UseCases.Pet.Register;
+using Petfolio.Application.UseCases.Pet.Update;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
@@ -12,7 +16,8 @@ namespace Petfolio.API.Controllers
     {
         [HttpPost]
         [ProducesResponseType(typeof(ResponseRegisteredPetJson), StatusCodes.Status201Created)]
-        public IActionResult Register([FromBody] RequestRegisterPetJson request)
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
+        public IActionResult Register([FromBody] RequestPetJson request)
         {
             var useCase = new RegisterPetUseCase();
 
@@ -20,5 +25,59 @@ namespace Petfolio.API.Controllers
 
             return Created(string.Empty, response);
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
+        public IActionResult Update([FromRoute]int id, [FromBody] RequestPetJson request)
+        {
+            var useCase = new UpdatePetUseCase();
+            useCase.Execute(id,request);
+            return NoContent();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ResponseAllPetJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult GetAll()
+        {
+            var useCase = new GetAllPetsUseCase();
+
+            var response = useCase.Execute();
+
+            if (response.Pets.Any())
+            {
+                return Ok(response);
+            }
+
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(ResponsePetJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            var useCase = new GetPetByIdUseCase();
+
+            var response = useCase.Execute(id);
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int id)
+        {
+            var useCase = new DeletePetByIdUseCase();
+            useCase.Execute(id);
+            return NoContent();
+        }
+
     }
+
 }
---
+++ Petfolio.Application/UseCases/Pet/GetById/GetPetByIdUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pet.GetById
+{
+    public class GetPetByIdUseCase
+    {
+        public ResponsePetJson Execute(int id)
+        {
+            return new ResponsePetJson
+            {
+                Id = id,
+                Name = "Pipoca",
+                Type = Communication.Enums.PetType.Cat,
+                Birthday = new DateTime(year: 2023, month: 01, day: 01)
+            };
+        }
+    }
+}
